9ab81df3b1be862ebd36bdc55500915580fc52ec
remkst777 <31829395+remkst777@users.noreply.github.com>
2019-05-25 13:50:59 +0300

    hover effect

--- src/index.js
+++ src/index.js
@@ -1,18 +1,58 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import styled from "styled-components";
 
 import kiwiExternal from "./kiwi.svg?external";
 import kiwiInline from "./kiwi.svg?inline";
 
-const cell = '150px';
+const cell = "150px";
+
+const IconHover = ({ color }) => `
+  .fill {
+    fill: ${color};
+  }
+
+  .stroke {
+    stroke: ${color};
+  }
+`;
+
+const IconStyled = styled.span`
+  display: inline-block;
+  width: 150px;
+  height: 150px;
+  margin-right: ${x => (x.margin ? 5 : 0)}px;
+
+  ${x => (x.hover ? `:hover { ${IconHover({ color: x.hover })} }` : ``)};
+`;
+
+const Button = styled.button`
+  display: flex;
+  align-items: center;
+
+  :hover {
+    color: red;
+
+    ${IconStyled} {
+      ${IconHover({ color: "red" })};
+    }
+  }
+`;
+
+const Icon = ({ icon, hover }) => (
+  <IconStyled hover={hover} dangerouslySetInnerHTML={{ __html: icon }} />
+);
 
 ReactDOM.render(
   <div>
+    <h4>SVG as part of button</h4>
+    <Button>
+      <Icon icon={kiwiExternal} />
+      <span>Hello world</span>
+    </Button>
+
     <h4>SVG as insertion</h4>
-    <div
-      style={{ width: cell, height: cell }}
-      dangerouslySetInnerHTML={{ __html: kiwiExternal }}
-    />
+    <Icon icon={kiwiExternal} hover="blue" />
 
     <h4>SVG as IMG</h4>
     <img width={cell} src={kiwiInline} alt="kiwi" />
